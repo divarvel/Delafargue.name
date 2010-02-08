c61383f58cb38512b1adcc44dbf9008766e8a7bf
Clément Delafargue <divarvel@gmail.com>
2010-02-08 15:00:04 +0100

    Change: Passage du titre en h1, et des templates en html

--- input/resource/script.js
+++ input/resource/script.js
@@ -1,23 +1,15 @@
-var pwnzor = function(color){
-  var content = document.getElementById("content");
-  if(color == 1)
-    content.style.background = "#FF9900";
-  else
-    content.style.background = "#DDDDDD";
-  
-  setTimeout(pwnzor(1 - color), 1000);
-}
-
 if (window.addEventListener) {
-  var sequence = [], konamiCode = "38,38,40,40,37,39,37,39,66,65";
+  var sequence = []
+  var konamiCode = "38,38,40,40,37,39,37,39,66,65";
   window.addEventListener("keydown", function(e){
     sequence.push( e.keyCode );
     if ( sequence.toString().indexOf( konamiCode ) >= 0 ){
       // Trucs importants
       var header = document.getElementById("header");
       header.getElementsByTagName("a")[0].innerHTML = "Clément 'Kevin' Delafargue";
-      header.getElementsByTagName("a")[0].href = "http://www.goatse.fr";
-      pwnzor(1);
+      
+      
+      
       sequence = [];
     }
   }, true);
